4e381d678e50088dfc66878ca28b9a508709360e
sy0227 <tv0227@naver.com>
2021-04-20 22:19:46 +0900

    solve : BOJ commit

---
+++ BOJ/Main_G1_11401.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Main_G1_11401 {
+	static final int MOD = 1000000007;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		long ans = nCr(N, K, MOD);
+		System.out.println(ans);
+	}
+	private static long nCr(int n, int r, int mod) {
+		if(r == 0) return 1L;
+		long[] fac = new long[n+1];
+		fac[0] = 1;
+		
+		for(int i = 1 ; i<=n ; i++) {
+			fac[i] = fac[i-1]*i%mod;
+		}
+		return (fac[n]*power(fac[r], mod-2, mod)%mod*power(fac[n-r], mod-2, mod)%mod)%mod;
+	}
+
+	private static long power(long x, long y, long p) {
+		long res = 1L;
+		x %= p;
+		while(y>0) {
+			if(y%2 == 1) res = (res*x)%p;
+			y = y>>1;
+			x = (x*x)%p;
+		}
+		return res;
+	}
+
+}
