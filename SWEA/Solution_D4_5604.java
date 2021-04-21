3d2ece3f2dc9b9965e0eeeaf5ac72bb2d95f6e0c
sy0227 <tv0227@naver.com>
2021-04-21 15:23:22 +0900

    solve : SWEA commit

--- SWEA/Solution_D4_5604.java
+++ SWEA/Solution_D4_5604.java
@@ -1,59 +1,55 @@
 package swea;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Solution_D4_5604 {
 	static long nums[];
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for(int test = 1 ; test<=T ; test++) {
 			long A = sc.nextLong();
 			long B = sc.nextLong();
 			nums = new long[10];
-			func(A, B);
+			long mul = 1;
+			
+			while(A<=B) {
+				// B는 끝자리 9가 되도록 --
+				while(B%10 != 9 && A<=B) {
+					saveToNum(B, mul);
+					B--;
+				}
+				if(A>B) break;
+				// A는 끝자리 0이 되도록 ++
+				while(A%10 != 0 && A<=B) {
+					saveToNum(A, mul);
+					A++;
+				}
+				
+				A /= 10;
+				B /= 10;
+				
+				// (B-A+1)*mul
+				for(int i = 0 ; i<10 ; i++) {
+					nums[i] += (B-A+1)*mul;
+				}
+				mul *= 10;
+			}
 			long ans = 0;
-			for (long num : nums) {
-				ans += num;
+			for(int i = 0 ; i<10 ; i++) {
+				ans += nums[i]*i;
 			}
 			System.out.println("#"+test+" "+ans);
 		}
 
 	}
 
-	private static void func(long A, long B) {
-		long cnt = 0;
-		long mul = 1;
-		
-		while(A<=B) {
-			// A는 끝자리 0이 되도록 ++
-			while(A%10 != 0 && A<=B) {
-				saveToNum(A, mul);
-				A++;
-			}
-			// B는 끝자리 9가 되도록 --
-			while(B%10 != 9 && A<=B) {
-				saveToNum(B, mul);
-				B--;
-			}
-			// (B/10-A/10+1)*mul
-			for(int i = 0 ; i<10 ; i++) {
-				nums[i] += (B/10-A/10+1)*mul;
-			}
-			mul *= 10;
-			A /= 10;
-			B /= 10;
-		}
-	}
-
 	private static void saveToNum(long num, long mul) {
 		// 해당 수 개수 ++
 		while(num>0) {
-			int idx = (int) (num%10);
+			String s = String.valueOf(num);
+			int idx = s.charAt(s.length()-1)-'0';
 			nums[idx] += mul;
 			num /= 10;
 		}
